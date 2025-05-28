d5712cbc2693313278b8d07a8cdcccc80b1c1bea
Benedito-Dev <beneditobittencourtt@gmail.com>
2025-05-27 21:43:05 -0300

    AuthWelcome

--- Mobile/app/index.jsx
+++ Mobile/app/index.jsx
@@ -7,16 +7,3 @@ export default function App() {
     <AuthWelcome/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center", // centraliza verticalmente
-    alignItems: "center",     // centraliza horizontalmente
-    backgroundColor: "#fff",  // fundo branco
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: "bold",
-  },
-});
--- Mobile/app/screens/AuthWelcome/styles.js
+++ Mobile/app/screens/AuthWelcome/styles.js
@@ -1,32 +1,70 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
+const { height } = Dimensions.get('window');
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "space-center", // centraliza verticalmente
-    alignItems: "center",     // centraliza horizontalmente
-    backgroundColor: "#fff",  // fundo branco
+    backgroundColor: '#fff',
   },
-  text: {
-    fontSize: 24,
-    fontWeight: "bold",
+  Imagecontainer: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    width: '100%',
+    backgroundColor: '#5f65d9',
+    opacity: 1,
+    zIndex: 20,
+  },
+  topImage: {
+    height: height * 0.50, // Ocupa 45% da tela
+    width: '130%',
+    marginLeft: -120, // Para estender a imagem além da tela
+    zIndex: 1,
+    opacity: 0.5, // Opacidade da imagem de fundo
+  },
+  content: {
+    flex: 1,
+    alignItems: 'center',
+    paddingTop: 20,
   },
   logo: {
-    width: 200,   // largura da imagem
-    height: 200,  // altura da imagem
-    resizeMode: "contain", // evita distorção da imagem
-    marginTop: 80,
+    width: 160,
+    height: 160,
+    marginTop: -100, // Para sobrepor a imagem de fundo
+    marginBottom: 10,
+    zIndex: 100,
+    backgroundColor: '#fff',
+    padding: 10,
+    borderRadius: 20, // Para deixar a imagem circular/
   },
-  containerDown: {
-    position: "absolute",       // Posiciona em relação à tela
-    bottom: 0,                  // Gruda na base da tela
-    width: "100%",              // Ocupa toda a largura
-    height: "50%",              // Até 50% da tela
-    backgroundColor: "#4942ce",
-    borderTopLeftRadius: 30,    // Arredonda canto superior esquerdo
-    borderTopRightRadius: 30,   // Arredonda canto superior direito
-    padding: 20,                // Espaçamento interno (opcional)
-    alignItems: "center",       // Centraliza conteúdo horizontalmente
-    justifyContent: "center",   // Centraliza conteúdo verticalmente
-}
+  subtitle: {
+    fontSize: 14,
+    color: '#666',
+  },
+  subtitleBold: {
+    fontSize: 14,
+    color: '#666',
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#1c1c3c',
+    paddingVertical: 12,
+    paddingHorizontal: 80,
+    borderRadius: 20,
+    marginTop: 30,
+  },
+  buttonSecondary: {
+    backgroundColor: '#1c1c3c',
+    paddingVertical: 12,
+    paddingHorizontal: 40,
+    borderRadius: 20,
+    marginTop: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontSize: 18,
+  },
 });
