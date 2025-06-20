d3884ded9bb729537c73dc46de61192df266c1fa
Benedito <beneditobittencourtt@gmail.com>
2025-06-20 16:59:43 -0300

    Medicamentos

--- Mobile/app/services/medicamentosService.js
+++ Mobile/app/services/medicamentosService.js
@@ -3,7 +3,10 @@ import api from '../api/api'
 export const getMedicametos = async () => {
     try {
         const response = await api.get('/medicamentos');
-        console.log(response)
+        console.log(response.data
+
+            
+        )
 
         return response.data;
     } catch (error) {
