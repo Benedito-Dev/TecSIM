b879c79736a57f30e0eff30c9e44bdaa7e34e159
EricBackEnd <rabelomateus4@gmail.com>
2025-07-08 18:38:45 -0300

    wip

--- Mobile/app/screens/home/Profile/PrivacyProfile/PrivacyScreen.jsx
+++ Mobile/app/screens/home/Profile/PrivacyProfile/PrivacyScreen.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, Text, ScrollView, TouchableOpacity, Alert } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, ScrollView, TouchableOpacity, Alert, Platform } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 import { useAuth } from '../../../../context/AuthContext';
@@ -12,36 +12,52 @@ export default function PrivacyScreen() {
   const navigation = useNavigation();
   const { user } = useAuth();
 
+
   const handleInactiveAccount = () => {
-    Alert.alert(
-      'Conta Inativa',
-      'Sua conta foi desativada. Você poderá reativá-la ao fazer login novamente.',
-      [
-        { text: 'OK', onPress: () => {
-          inativarPaciente(user.id)
-          navigation.reset({ index: 0, routes: [{ name: 'Welcome' }] });
-        } }
-      ]
-    );
-  }
+    if (Platform.OS === 'web') {
+      inativarPaciente(user.id);
+      alert('Conta Inativa', 'Sua conta foi desativada. Você poderá reativá-la ao fazer login novamente.')
+      navigation.reset({ index: 0, routes: [{ name: 'Welcome' }] });
+    } else {
+      Alert.alert(
+        'Conta Inativa',
+        'Sua conta foi desativada. Você poderá reativá-la ao fazer login novamente.',
+        [
+          {
+            text: 'OK',
+            onPress: () => {
+              inativarPaciente(user.id);
+              navigation.reset({ index: 0, routes: [{ name: 'Welcome' }] });
+            },
+          },
+        ]
+      );
+    }
+  };
 
   const handleDeleteAccount = () => {
-    Alert.alert(
-      'Excluir Conta',
-      'Tem certeza que deseja excluir sua conta? Essa ação é irreversível.',
-      [
-        { text: 'Cancelar', style: 'cancel' },
-        {
-          text: 'Excluir',
-          style: 'destructive',
-          onPress: () => {
-            deletePaciente(user.id)
-            Alert.alert('Conta excluída', 'Sua conta foi excluída com sucesso.');
-            navigation.reset({ index: 0, routes: [{ name: 'Welcome' }] });
+    if (Platform.OS === 'web') {
+      deletePaciente(user.id);
+      alert('Conta excluída', 'Sua conta foi excluída com sucesso.')
+      navigation.reset({ index: 0, routes: [{ name: 'Welcome' }] });
+    } else {
+      Alert.alert(
+        'Excluir Conta',
+        'Tem certeza que deseja excluir sua conta? Essa ação é irreversível.',
+        [
+          { text: 'Cancelar', style: 'cancel' },
+          {
+            text: 'Excluir',
+            style: 'destructive',
+            onPress: () => {
+              deletePaciente(user.id);
+              Alert.alert('Conta excluída', 'Sua conta foi excluída com sucesso.');
+              navigation.reset({ index: 0, routes: [{ name: 'Welcome' }] });
+            },
           },
-        },
-      ]
-    );
+        ]
+      );
+    }
   };
 
   return (
