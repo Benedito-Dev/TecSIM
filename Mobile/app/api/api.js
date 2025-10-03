aafbfa7438a196985a4d05d494d7ae84abf1fca6
Lhara <lhararaysa23@gmail.com>
2025-10-03 18:12:12 -0300

    edção do logo

--- Mobile/app/api/api.js
+++ Mobile/app/api/api.js
@@ -3,7 +3,7 @@ import { IP_HOST } from '@env';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Para desenvolvimento com backend local
-const API_URL = `http://192.168.1.104:3000`; // Utilizar IP da Maquina ao inves de localhost
+const API_URL = `http://10.0.30.181:3000`; // Utilizar IP da Maquina ao inves de localhost
 
 // Para Android emulador, use:
 // const API_URL = 'http://10.0.2.2:3000';
