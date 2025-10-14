980dfa35e9ea484d71b076103a98ca5bdfbacdca
Benedito <beneditobittencourtt@gmail.com>
2025-10-13 22:08:57 -0300

    wip

--- Mobile/android/app/src/main/java/com/seunome/tecsim/MainActivity.kt
+++ Mobile/android/app/src/main/java/com/seunome/tecsim/MainActivity.kt
@@ -36,7 +36,7 @@ class MainActivity : ReactActivity() {
   override fun createReactActivityDelegate(): ReactActivityDelegate {
     return ReactActivityDelegateWrapper(
           this,
-          false,
+          BuildConfig.IS_NEW_ARCHITECTURE_ENABLED,
           object : DefaultReactActivityDelegate(
               this,
               mainComponentName,
